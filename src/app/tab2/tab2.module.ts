54d4ce4d833821f420ec29d472dc2c661390fff1
Elise Nguyen <elisengu@usc.edu>
2019-08-02 17:07:17 -0700

    first ionic app

---
+++ src/app/tab2/tab2.module.ts
@@ -0,0 +1,38 @@
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+export class Tab2Page {
+  currentImage: any;
+
+  constructor(private camera: Camera) { }
+}
+takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+     console.log("Camera issue:" + err);
+    });
+  }
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab2Page } from './tab2.page';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: Tab2Page }])
+  ],
+  declarations: [Tab2Page]
+})
+export class Tab2PageModule {}
